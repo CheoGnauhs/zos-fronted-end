2583d3c7d07c9ff8562bde235170044d948f0071
yansong <yansongsongsong@gmail.com>
2018-07-04 16:17:52 +0800

    add one error

--- src/pages/mastercatalog.vue
+++ src/pages/mastercatalog.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card class="box-card">
+  <el-card class="box-card" v-loading="loading">
     <div slot="header">
       <el-row :gutter="20">
         <el-col :span="6">
@@ -30,31 +30,58 @@
 <script>
 export default {
   data () {
-      this.chartSettings = {
-        selectedMode: 'single',
-        hoverAnimation: false
+    this.chartSettings = {
+      selectedMode: 'single',
+      hoverAnimation: true
+    }
+    var self = this
+    this.chartEvents = {
+      click: function (e) {
+        self.name = e.name
+        console.log(e)
       }
-      var self = this
-      this.chartEvents = {
-        click: function (e) {
-          self.name = e.name
-          console.log(e)
-        }
-      }
-      return {
-        chartData: {
-          columns: ['日期', '访问用户'],
-          rows: [
-            { '日期': '1/1', '访问用户': 1393 },
-            { '日期': '1/2', '访问用户': 3530 },
-            { '日期': '1/3', '访问用户': 2923 },
-            { '日期': '1/4', '访问用户': 1723 },
-            { '日期': '1/5', '访问用户': 3792 },
-            { '日期': '1/6', '访问用户': 4593 }
-          ]
-        },
-        name: ''
-      }
+    }
+    return {
+      chartData: {
+        columns: ['日期', '访问用户'],
+        rows: [
+          { '日期': '1/1', '访问用户': 1393 },
+          { '日期': '1/2', '访问用户': 3530 },
+          { '日期': '1/3', '访问用户': 2923 },
+          { '日期': '1/4', '访问用户': 1723 },
+          { '日期': '1/5', '访问用户': 3792 },
+          { '日期': '1/6', '访问用户': 4593 }
+        ]
+      },
+      name: 'to be selectd',
+      loading: true,
+      rawData: {}
+    }
+  },
+
+  methods: {
+     fetchData(){
+      this.$http.post('http://localhost:8888/upload', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        }).then((response) => {
+            // 成功回调
+            this.raw = response.body;
+            console.log(this.raw);
+            this.loading = false;
+        }, (response) => {
+            // 响应错误回调
+            this.fetchDataError("获取数据失败");
+        });
+
+    },
+
+    fetchDataError(str) {
+        this.$message.error(str);
+    }
+  },
+
+  mounted(){
+    this.fetchData();
   }
 };
 </script>
