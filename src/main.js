fa9003eeea6fe9e50b63e5192388d9752665d5b4
yansong <yansongsongsong@gmail.com>
2018-07-04 16:19:24 +0800

    add vue resource

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui';
 import VCharts from 'v-charts'
+import VueResource from 'vue-resource'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
@@ -13,6 +14,7 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 Vue.use(VCharts);
+Vue.use(VueResource);
 
 [VeLine, VeMap].forEach(comp => {
   Vue.component(comp.name, comp);
